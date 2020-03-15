f6ef20f5d3553e949444c8643d4183e837e60d57
fanux <fhtjob@hotmail.com>
2020-03-15 14:56:23 +0800

    delete

--- example/go-restful/run-bin.sh
+++
@@ -1,4 +0,0 @@
-rm robot
-docker run --rm -v /Users/fanux/work/src/github.com/fanux/robot:/go/src/github.com/fanux/robot \
-  -w /go/src/github.com/fanux/robot/example/go-restful golang:1.12.7  go build -o robot
-scp robot root@store.lameleg.com:/root
